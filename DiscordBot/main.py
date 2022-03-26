add8973a46cde6ac8d464a03b9cd541313992b8b
MadKamel <rustrumek@gmail.com>
2022-03-26 15:52:40 -0700

    Added Discord B0t

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -1,3 +1,15 @@
 import discord, os
 
-tokenfile = open("../token.ignore", "r") --token go brrr
+tokenfile = open("../token.ignore", "r") #token go brrr
+
+token = tokenfile.read()
+
+
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+	print(f'{client.user} is ready.')
+
+client.run(token)
