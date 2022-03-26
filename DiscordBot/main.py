1c7a3445ee975e882285eccfeac626be39603611
MadKamel <rustrumek@gmail.com>
2022-03-26 16:02:44 -0700

    Bot testing functions

--- DiscordBot/main.py
+++ DiscordBot/main.py
@@ -12,4 +12,10 @@ client = discord.Client()
 async def on_ready():
 	print(f'{client.user} is ready.')
 
+async def on_message():
+    print('A user speaks... Reply.')
+    if False #message.author == client.user:
+        return
+    await messange.channel.send("https://tenor.com/view/laugh-neptune-neptunia-nep-gif-7234123")
+
 client.run(token)
